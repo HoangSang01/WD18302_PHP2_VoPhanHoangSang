84d9ae76f3a13d72f519e840df549d326cbf3742
Aki <yuukirito0107@gmail.com>
2024-01-14 17:56:16 +0700

    2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'admin/wd18302_php2_vo-phan-hoang-sang',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '74c261a46dd2c6aabe5db56f7ae0739cfd0065a6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'admin/wd18302_php2_vo-phan-hoang-sang' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '74c261a46dd2c6aabe5db56f7ae0739cfd0065a6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
