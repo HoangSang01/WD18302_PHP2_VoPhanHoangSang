4d85a9cfa1a71f30a8e7f9d94cc05a2470e42222
Aki <yuukirito0107@gmail.com>
2024-01-18 18:00:16 +0700

    Update HomeController.php

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -4,6 +4,10 @@ namespace App\Controller;
 
 class HomeController
 {
+    function HomeController()
+    {
+        $this->homePage();
+    }
     function homePage()
     {
         echo 'this is home';
